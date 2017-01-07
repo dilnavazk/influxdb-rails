32bd19004d21aba2d24956045716a96389edbf3c
Dilnavaz K <dilnavaz@pictink.com>
2017-01-07 20:18:01 +0530

    Changed alias_method_chain to prepend

--- lib/influxdb/rails/middleware/hijack_render_exception.rb
+++ lib/influxdb/rails/middleware/hijack_render_exception.rb
@@ -3,16 +3,16 @@ module InfluxDB
     module Middleware
       module HijackRenderException
         def self.included(base)
-          base.prepend(:render_exception, :influxdb)
+          base.prepend HijackRenderException
         end
 
-        def render_exception_with_influxdb(env, e)
+        def render_exception(env, e)
           controller = env["action_controller.instance"]
           request_data = controller.try(:influxdb_request_data) || {}
           unless InfluxDB::Rails.configuration.ignore_user_agent?(request_data[:user_agent])
             InfluxDB::Rails.report_exception_unless_ignorable(e, request_data)
           end
-          render_exception_without_influxdb(env, e)
+          super(env, e)
         end
       end
     end
